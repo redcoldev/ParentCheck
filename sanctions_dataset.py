964216f03040db1f4c2a51ef38e47c679c0d0e2d
RedCol Development Git <grantmurray7@gmail.com>
2025-12-05 09:45:19 +0000

    Create sanctions_dataset.py

---
+++ sanctions_dataset.py
@@ -0,0 +1,314 @@
+SANCTIONS_DATA = {
+    "United Arab Emirates": [
+        "National Terrorist List"
+    ],
+    "Armenia": [
+        "Central Bank of Armenia – Domestic List"
+    ],
+    "Argentina": [
+        "Registro Público de Personas Vinculadas a Actos de Terrorismo y su Financiamiento – Sanctions"
+    ],
+    "Austria": [
+        "Oesterreichische National Bank"
+    ],
+    "Australia": [
+        "Australian National Security",
+        "Commonwealth Law",
+        "Department of Foreign Affairs and Trade"
+    ],
+    "Azerbaijan": [
+        "Financial Monitoring Service"
+    ],
+    "Bangladesh": [
+        "Financial Intelligence Unit – Domestic Sanctions List"
+    ],
+    "Belgium": [
+        "Royal Decree 28 December 2006"
+    ],
+    "Burkina Faso": [
+        "Advisory Commission on Administrative Freezing (CCGA) – National List"
+    ],
+    "Bulgaria": [
+        "Council of Ministers – Sanctions"
+    ],
+    "Bahrain": [
+        "Ministry of Information Affairs – Domestic Terrorist List"
+    ],
+    "Belarus": [
+        "State Security Agency – Sanctions"
+    ],
+    "Canada": [
+        "Public Safety Canada",
+        "Freezing Assets of Corrupt Foreign Officials Regulations – Tunisia",
+        "Justice for Victims of Corrupt Foreign Officials Regulations",
+        "Regulations Implementing UN Terrorism Resolutions",
+        "Special Economic Measures (Extremist Settler Violence)",
+        "Special Economic Measures (Hamas Terrorist Attacks)",
+        "Special Economic Measures – Belarus",
+        "Special Economic Measures – Burma",
+        "Special Economic Measures – Guatemala",
+        "Special Economic Measures – Haiti",
+        "Special Economic Measures – Iran",
+        "Special Economic Measures – Moldova",
+        "Special Economic Measures – Nicaragua",
+        "Special Economic Measures – China",
+        "Special Economic Measures – Russia",
+        "Special Economic Measures – South Sudan",
+        "Special Economic Measures – Sri Lanka",
+        "Special Economic Measures – Sudan",
+        "Special Economic Measures – Syria",
+        "Special Economic Measures – Ukraine",
+        "Special Economic Measures – Venezuela",
+        "Special Economic Measures – Zimbabwe"
+    ],
+    "Switzerland": [
+        "Federal Department of Foreign Affairs – Syria",
+        "State Secretariat for Economic Affairs"
+    ],
+    "Ivory Coast": [
+        "National Financial Information Processing Unit (CENTIF) – National Sanctions List"
+    ],
+    "China": [
+        "Ministry of Commerce – Countermeasures List",
+        "Ministry of Commerce – Unreliable Entity List",
+        "Ministry of Foreign Affairs – Sanctions Announcements",
+        "Ministry of Public Security – Sanctions",
+        "Taiwan Affairs Office – Sanctions"
+    ],
+    "Czech Republic": [
+        "Ministry of Foreign Affairs – National Sanctions List"
+    ],
+    "Algeria": [
+        "Ministry of Interior – National Sanctions List"
+    ],
+    "Estonia": [
+        "Ministry of Foreign Affairs – Sanctions"
+    ],
+    "Egypt": [
+        "Domestic Terrorist List"
+    ],
+    "European Union": [
+        "EU Consolidated List"
+    ],
+    "Hong Kong": [
+        "Monetary Authority – Sanctions",
+        "Safeguarding National Security Ordinance – Sanctions"
+    ],
+    "Indonesia": [
+        "Financial Transaction Reports and Analysis Center (PPATK) – Sanctions"
+    ],
+    "Israel": [
+        "Ministry of Finance – Sanctions",
+        "Ministry of Defense – Terrorism List"
+    ],
+    "India": [
+        "Ministry of Home Affairs – Sanctions"
+    ],
+    "Iraq": [
+        "Terrorist Freezing Funds Committee – International List",
+        "Terrorist Freezing Funds Committee – National List"
+    ],
+    "Jordan": [
+        "Technical Committee for Implementation of UN Resolutions – National List"
+    ],
+    "Japan": [
+        "Ministry of Finance – Sanctions"
+    ],
+    "Kenya": [
+        "Prevention of Terrorism Act – Specified Entities",
+        "Targeted Financial Sanctions – Domestic List"
+    ],
+    "Kyrgyzstan": [
+        "State Financial Intelligence Service – Sanctions"
+    ],
+    "South Korea": [
+        "Korea Financial Intelligence Unit",
+        "Ministry of Strategy and Finance – Sanctions",
+        "Prime Minister’s Office – Sanctions"
+    ],
+    "Kuwait": [
+        "Ministry of Foreign Affairs – National List"
+    ],
+    "Kazakhstan": [
+        "Financial Monitoring Agency – Terrorism/Extremism Financing",
+        "Financial Monitoring Agency – Terrorist Activities",
+        "Committee on Legal Statistics & Special Records – Sanctions"
+    ],
+    "Lebanon": [
+        "Internal Security Forces – National Terrorism Financial List"
+    ],
+    "Liechtenstein": [
+        "Financial Market Authority – Sanctions"
+    ],
+    "Sri Lanka": [
+        "Financial Intelligence Unit – UNSCR 1373"
+    ],
+    "Lithuania": [
+        "Financial Crime Investigation Service – Sanctions"
+    ],
+    "Latvia": [
+        "Financial Intelligence Unit – Sanctions",
+        "National Sanctions List"
+    ],
+    "Morocco": [
+        "Commission for UN Sanctions – Local List"
+    ],
+    "Monaco": [
+        "Service d’Information et de Contrôle sur les Circuits Financiers"
+    ],
+    "Moldova": [
+        "Interinstitutional Supervisory Council – Sanctions"
+    ],
+    "North Macedonia": [
+        "Financial Intelligence Office – Domestic List"
+    ],
+    "Mauritius": [
+        "National Sanctions Secretariat – Designated Parties"
+    ],
+    "Mexico": [
+        "UIF – Resolution 1267",
+        "UIF – Resolution 1718",
+        "UIF – Resolution 1988",
+        "UIF – Resolution 1737 & 2231"
+    ],
+    "Malaysia": [
+        "Ministry of Home Affairs – Sanctions"
+    ],
+    "Mozambique": [
+        "Financial Information Office – National Sanctions List"
+    ],
+    "Nigeria": [
+        "Sanctions Committee – Nigeria Sanctions List"
+    ],
+    "Netherlands": [
+        "National Terrorism List"
+    ],
+    "New Zealand": [
+        "Police List – UNSC 1267/1989/1988",
+        "Police List – UNSC 1373",
+        "Russia Sanctions Regulations"
+    ],
+    "Oman": [
+        "National Counter Terrorism Committee – Local List"
+    ],
+    "Philippines": [
+        "Anti-Terrorism Council – Designation Announcements"
+    ],
+    "Pakistan": [
+        "National Counter Terrorism Authority – Proscribed Organisations",
+        "National Counter Terrorism Authority – Proscribed Persons"
+    ],
+    "Poland": [
+        "General Inspector of Financial Information – Sanctions",
+        "Ministry of Interior – Sanctions"
+    ],
+    "Qatar": [
+        "National Terrorists Designation List"
+    ],
+    "Serbia": [
+        "Domestic List of Designated Persons"
+    ],
+    "Russia": [
+        "Federal Financial Monitoring Service",
+        "Federal Security Service (FSB) – Terrorist List",
+        "Presidential Sanctions Decree No. 1300",
+        "Presidential Sanctions Decree No. 252"
+    ],
+    "Rwanda": [
+        "Financial Intelligence Centre – Domestic Sanctions List"
+    ],
+    "Saudi Arabia": [
+        "Permanent Counter Terrorism Committee – National List"
+    ],
+    "Singapore": [
+        "Attorney-General’s Chambers – Terrorism",
+        "MAS – Measures Related to Russia",
+        "MAS – Measures Related to Violent Israeli Settlers"
+    ],
+    "Thailand": [
+        "Anti-Money Laundering Office – Sanctions"
+    ],
+    "Tajikistan": [
+        "Financial Intelligence Unit – National Bank"
+    ],
+    "Tunisia": [
+        "National Sanctions List"
+    ],
+    "Turkey": [
+        "Ministry of Treasury and Finance – Article 5",
+        "Ministry of Treasury and Finance – Article 6",
+        "Ministry of Treasury and Finance – Article 7"
+    ],
+    "Taiwan": [
+        "Ministry of Justice Investigation Bureau – Sanctions"
+    ],
+    "Ukraine": [
+        "National Security & Defense Council – Sanctions",
+        "State Financial Monitoring Service – Sanctions"
+    ],
+    "United Kingdom": [
+        "FCO – Sanctions List",
+        "HM Treasury – Financial Sanctions",
+        "HM Treasury – Investment Ban Targets",
+        "Belarus Sanctions (EU Exit) Regulations",
+        "Russia Sanctions (EU Exit) Regulations",
+        "Home Office – Proscribed Terrorist Groups"
+    ],
+    "United Nations": [
+        "UNSC Resolution 1267 / 1989 / 2253 – ISIL & Al-Qaida",
+        "UNSC Resolution 1988",
+        "UNSC Resolution 2140",
+        "UNSC Resolution 1533",
+        "UNSC Resolution 1718",
+        "UNSC Resolution 1718 – Designated Vessels",
+        "UNSC Resolution 1970",
+        "UNSC Resolution 2048",
+        "UNSC Resolution 1518 – Iraq",
+        "UNSC Resolution 1591 – Sudan",
+        "UNSC Resolution 1737 – Iran",
+        "UNSC Resolution 2127 – Central African Republic",
+        "UNSC Resolution 2206",
+        "UNSC Resolution 2653 – Haiti",
+        "UNSC Resolution 751 – Al-Shabaab"
+    ],
+    "United States": [
+        "Dept. of State – Cuba Prohibited Accommodations",
+        "Dept. of State – Sanctions",
+        "Dept. of State – Terrorist Exclusion List",
+        "Dept. of State – Foreign Terrorist Organizations",
+        "Executive Order 14064",
+        "OFAC – CAPTA Sanctions",
+        "OFAC – Directive 1 (EO 14038)",
+        "OFAC – Foreign Sanctions Evaders",
+        "OFAC – Non-SDN Chinese Military Companies",
+        "OFAC – Non-SDN Menu-Based Sanctions",
+        "OFAC – Sectoral Sanctions Identifications",
+        "BIS – Denied Persons List",
+        "Dept. of State – Chemical/Bio Weapons Sanctions",
+        "Dept. of State – Cuba Restricted List",
+        "Dept. of State – EO 12938",
+        "Dept. of State – EO 13382",
+        "Dept. of State – Export-Import Bank Act",
+        "Dept. of State – INKSNA Act",
+        "Dept. of State – Missile Sanctions Laws",
+        "Dept. of State – Nuclear Proliferation Prevention Act",
+        "Dept. of State – Transfer of Lethal Military Equipment",
+        "Dept. of State – CAATSA (Sections 231/235)",
+        "FinCEN – Section 311 Special Measures",
+        "OFAC – EO13808",
+        "OFAC – Palestinian Legislative Council",
+        "OFAC – SDN List"
+    ],
+    "Uzbekistan": [
+        "Department for Combating Economic Crimes – National List"
+    ],
+    "Vatican City": [
+        "Law N.XVIII – Article 71 Sanctions"
+    ],
+    "Vietnam": [
+        "Ministry of Public Security – Terrorist Entities"
+    ],
+    "South Africa": [
+        "Targeted Financial Sanctions List"
+    ]
+}
